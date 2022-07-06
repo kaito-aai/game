f0d313c2ea5423322e4faf1afc7372fc4d4d5201
kaito-aai <kaito.s0508@gmail.com>
2022-07-07 00:10:39 +0900

    gameの土台を作成

---
+++ Character.cs
@@ -0,0 +1,66 @@
+namespace Game {
+    public abstract class Character {
+        public string Name { get; set; }
+        public ushort HP { get; set; }
+        public ushort Attack { get; set; }
+        public ushort Defense { get; set; }
+
+        public Character(string name, ushort hp, ushort attack, ushort defense)
+        {
+            Name = name;
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public abstract void TakeDamage(ushort damage);
+
+        public abstract ushort GiveDamage(Character character);
+
+        public bool isAlive()
+        {
+            return HP > 0;
+        }
+    }
+
+    public class Human : Character {
+        public Human(string name, ushort hp, ushort attack, ushort defense) : base(name, hp, attack, defense)
+        {
+        }
+
+        public override void TakeDamage(ushort damage)
+        {
+            var dmg = damage - Defense;
+            if (dmg < 0) {
+                return;
+            }
+            var ushortDmg = Convert.ToUInt16(dmg);
+            HP -= ushortDmg;
+        }
+
+        public override ushort GiveDamage(Character character)
+        {
+            var beforeHp = character.HP;
+            character.TakeDamage(Attack);
+            return Convert.ToUInt16(beforeHp - character.HP);
+        }
+    }
+
+    public class Monster : Character {
+        public Monster(string name, ushort hp, ushort attack, ushort defense) : base(name, hp, attack, defense)
+        {
+        }
+
+        public override void TakeDamage(ushort damage)
+        {
+            HP -= damage;
+        }
+
+        public override ushort GiveDamage(Character character)
+        {
+            var beforeHp = character.HP;
+            character.TakeDamage(Attack);
+            return Convert.ToUInt16(beforeHp - character.HP);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Game;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Game");
+Console.Write("Enter Character Name >>");
+var name = Console.ReadLine() ?? "HUMAN";
+
+var userCharacter = new Human(name, 100, 10, 10);
+var monster = new Monster("Ghost", 100, 5, 5);
+var turnCount = 0;
+
+while (userCharacter.isAlive() && monster.isAlive()) {
+    turnCount++;
+    Console.WriteLine($"=====================TURN: {turnCount}======================");
+    Console.WriteLine();
+    Console.Write("Your Turn: Attack[A] / Nothing[N]");
+    var turn = Console.ReadKey();
+    Console.WriteLine();
+    if (turn.KeyChar == 'A') {
+        var monsterDamage = userCharacter.GiveDamage(monster);
+        Console.WriteLine($"Your Turn: {monster.Name} damaged {monsterDamage}!!");
+    }
+
+    if (turn.KeyChar == 'N') {
+        Console.WriteLine("Your Turn: You do nothing");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"Interval: {userCharacter.Name}: {userCharacter.HP} / {monster.Name}: {monster.HP}");
+    if (!monster.isAlive()) {
+        break;
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("Enemy Turn");
+
+    var userDamage = monster.GiveDamage(userCharacter);
+    Console.WriteLine($"Enemy Turn: {userCharacter.Name} damaged {userDamage}!!");
+
+    Console.WriteLine($"Interval: {userCharacter.Name}: {userCharacter.HP} / {monster.Name}: {monster.HP}");
+    Console.WriteLine();
+}
+
+if (!userCharacter.isAlive()) {
+    Console.WriteLine("You Lose");
+    Console.ReadKey();
+    return;
+}
+
+Console.WriteLine("You Win!!");
+Console.ReadKey();
+return;
